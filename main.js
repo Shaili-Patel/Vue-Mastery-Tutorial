7a20a16ef2e81fa36cf0cf56d7a68a885ed67005
Shaili Patel <526shailipatel@frhsd.com>
2025-11-25 13:30:56 -0500

    Attribute Binding

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg'
         }
     }
 })
+
 
